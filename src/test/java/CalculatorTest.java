5c7f23fb65d7ef30e68505d39041870a6937c562
massi-prog-code <harveysmithforever1998@gmail.com>
2026-03-09 15:29:58 +0100

    comprendre la démarche en utilisant la classe Calculator

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+import org.example.Calculator;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2,3));
+    }
+    @Test
+    void testSubstract() {
+        assertEquals(2, calculator.subtract(3,1));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(9, calculator.multiply(3,3));
+    }
+
+
+    @Test
+    void testDivide() {
+        assertEquals(2, calculator.divide(4,2));
+    }
+    @Test
+    void testDivideByzero(){
+        assertThrows(ArithmeticException.class,()->{
+            calculator.divide(4,0);
+        });
+    }
+}
